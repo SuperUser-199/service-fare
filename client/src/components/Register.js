7abec122aa1156dd7d980c4f055696c5c49ba204
Chetan Kaushik <kaushikchetan199@gmail.com>
2021-10-15 10:04:55 +0530

    Register and Login page made

---
+++ client/src/components/Register.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Style.css'
+
+function Register() {
+    return (
+        <div class="container">
+        <div class="logo">
+            <h3>Service Fare</h3>
+        </div>
+            <div class="register-form toggle">
+                <h2>Register</h2>
+                <form autocomplete="off">
+                    <input type="text" name="reg-fname" id="reg-fname" required placeholder="Enter your first name"/>
+                    <input type="text" name="reg-lname" id="reg-lname" required placeholder="Enter your last name"/>
+                    <input type="email" name="register-ml" id="register-ml" required placeholder="Enter your mail"/>
+                    <input type="number" name="mobno" id="mobno" required placeholder="Enter your mobile number"/>
+                    <input type="password" name="register-pd" id="register-pd" required placeholder="Enter your password"/>
+                    <input type="password" name="register-cpd" id="register-cpd" required placeholder="Confirm your password"/>
+                    <div class="register-row">
+                        
+                    </div>
+                    <button type="submit">Register</button>
+                    <p>Do you have an account? <a href="/" class="switch">Login Now</a></p>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Register
